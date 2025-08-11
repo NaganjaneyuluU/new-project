465949d27cf0b66cc55a5270fd6354ed770b24a4
unaganjaneyulu <naganjaneyulu@snovasys.com>
2025-08-11 22:21:50 +0530

    pplion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Index } from './index';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'header', component: Header },
+     { path: 'footer', component: Footer },
+     {path:'index',component:Index}
+];
