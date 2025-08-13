e005f60bc1f07ab43cc6b6da55d1c42267022621
Sushma <sushma@snovasys.com>
2025-08-14 00:58:33 +0530

    added contactus

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Footer } from './components/footer-components/footer/footer';
 import { Index } from './components/index';
 import { Header } from './components/header/header';
+import { ContactUs } from './components/footer-components/contact-us/contact-us';
 
 
 export const routes: Routes = [
      { path: 'footer', component:Footer },
      {path:'index',component:Index},
-     {path:'header',component:Header}
+     {path:'header',component:Header},
+     {path:'contactus',component:ContactUs}
 ];
