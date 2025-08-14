c0a8c6a3594e840d853f4b6bd3ccdfd95391a247
unaganjaneyulu <naganjaneyulu@snovasys.com>
2025-08-14 23:19:58 +0530

    new page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { Footer } from './components/footer-components/footer/footer';
 import { Index } from './components/index';
 import { Header } from './components/header/header';
+import { Imprint } from './components/footer-components/imprint/imprint';
 
 
 export const routes: Routes = [
      { path: 'footer', component:Footer },
      {path:'index',component:Index},
-     {path:'header',component:Header}
+     {path:'header',component:Header},
+    { 
+    path: 'footer-components',
+    children: [
+      { path: 'footer', component: Footer },
+      { path: 'imprint', component: Imprint }
+    ]
+  },
 ];
