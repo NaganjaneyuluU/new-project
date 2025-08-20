3f28562de39f015e0722bd774f33bb0e72b26dd8
unaganjaneyulu <naganjaneyulu@snovasys.com>
2025-08-20 22:38:22 +0530

    footer added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, HostListener, signal } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { Footer } from './components/footer-components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Header,RouterModule],
+  imports: [RouterOutlet,Header,RouterModule,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/index/index.ts
+++ src/app/components/index/index.ts
@@ -1,13 +1,12 @@
 import { Component, HostListener } from '@angular/core';
 import { MACHINES } from '../../../dataset/cards-data';
-import { Footer } from '../footer-components/footer/footer';
 import { machines } from '../../../dataset/cards-data';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-index',
   standalone:true,
-  imports: [CommonModule, Footer,RouterModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './index.html',
   styleUrl: './index.css'
 })
